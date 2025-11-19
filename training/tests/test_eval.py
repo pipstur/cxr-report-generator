19f79d1e0d178a889dc130b51dfdef83d1182b46
Vojislav S <vojislavstevanovic171@gmail.com>
2025-11-19 22:37:44 +0100

    update: with logger

--- training/tests/test_eval.py
+++ training/tests/test_eval.py
@@ -22,6 +22,7 @@ def test_train_eval(tmp_path: Path, cfg_train: DictConfig, cfg_eval: DictConfig)
     assert str(tmp_path) == cfg_train.paths.output_dir == cfg_eval.paths.output_dir
 
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.max_epochs = 1
         cfg_train.test = True
--- training/tests/test_train.py
+++ training/tests/test_train.py
@@ -18,6 +18,7 @@ def test_train_fast_dev_run(cfg_train: DictConfig) -> None:
     HydraConfig().set_config(cfg_train)
     generate_dummy_chexpert_dataset(base_dir=cfg_train.data.data_dir)
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.accelerator = "cpu"
@@ -33,6 +34,7 @@ def test_train_fast_dev_run_gpu(cfg_train: DictConfig) -> None:
     """
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.accelerator = "gpu"
@@ -50,6 +52,7 @@ def test_train_epoch_gpu_amp(cfg_train: DictConfig) -> None:
     HydraConfig().set_config(cfg_train)
     generate_dummy_chexpert_dataset(base_dir=cfg_train.data.data_dir)
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.max_epochs = 1
         cfg_train.trainer.accelerator = "gpu"
@@ -67,6 +70,7 @@ def test_train_epoch_double_val_loop(cfg_train: DictConfig) -> None:
     HydraConfig().set_config(cfg_train)
     generate_dummy_chexpert_dataset(base_dir=cfg_train.data.data_dir)
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.max_epochs = 1
         cfg_train.trainer.val_check_interval = 0.5
@@ -83,6 +87,7 @@ def test_train_ddp_sim(cfg_train: DictConfig) -> None:
     HydraConfig().set_config(cfg_train)
     generate_dummy_chexpert_dataset(base_dir=cfg_train.data.data_dir)
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.max_epochs = 2
         cfg_train.trainer.accelerator = "cpu"
@@ -100,6 +105,7 @@ def test_train_resume(tmp_path: Path, cfg_train: DictConfig) -> None:
     :param cfg_train: A DictConfig containing a valid training configuration.
     """
     with open_dict(cfg_train):
+        cfg_train.logger = "tensorboard"
         cfg_train.data.batch_size = 1
         cfg_train.trainer.max_epochs = 1
         cfg_train.trainer.limit_val_batches = 1.0
