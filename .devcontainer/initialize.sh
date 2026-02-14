e7123f7a39f3987b3931243376fbfb1577a2a276
Vojislav Stevanović <95634547+pipstur@users.noreply.github.com>
2026-02-14 21:29:17 +0100

    update: devcontainer setup process (#9)
    * update: initialize.sh script to include different handling of SSH keys
    * update: devcontainer to mount local .ssh folder

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 set -e
-sudo chown -R vscode:vscode /workspaces/cxr-report-generator/
+sudo rm -f /etc/apt/sources.list.d/yarn.list
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get update
-sudo apt-get install -y git-lfs libgl1
-sudo apt install openssh-client
-git lfs install
-python -m venv venv
-source venv/bin/activate
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source ~/.bashrc
-pip install --upgrade pip
-# pre-commit install # optionally, if you plan to push code
-uv pip install --extra-index-url https://download.pytorch.org/whl/cu126 --index-strategy unsafe-best-match -r training/requirements.txt
+sudo apt-get install -y git-lfs libgl1 openssh-client
 
-sudo chmod 700 ~/.ssh && sudo chmod 600 ~/.ssh/*
+git lfs install
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+hash -r
+
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python -m venv .venv
+else
+    echo "Virtual environment already exists, skipping..."
+fi
+
+source .venv/bin/activate
+
+uv pip install \
+  --extra-index-url https://download.pytorch.org/whl/cu126 \
+  --index-strategy unsafe-best-match \
+  -r training/requirements.txt
+
+.venv/bin/pre-commit install || true
+
 mkdir -p ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+sudo chown -R vscode:vscode ~/.ssh
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519.pub
+chmod 644 ~/.ssh/known_hosts
+ssh -T git@github.com || true
